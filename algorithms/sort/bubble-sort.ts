23abf7bd88000be6a974ef7fe130178e7984815d
Paulo Marinho <pakas_vieira_95@hotmail.com>
2022-01-08 11:07:30 -0300

    feat(sort): add bubble-sort code

---
+++ algorithms/sort/bubble-sort.ts
@@ -0,0 +1,26 @@
+// INFO: Run 'npx ts-node bubble-sort.ts' on prompt to execute or
+// run "Debug Current File" if using vscode :)
+
+// INFO: This code is really inefficient because seems like always
+// run in the worst case of insertion-sort (TODO: confirm that on pg 41)
+
+// TODO: What is the name of those approach?
+
+// TODO: Add infos about these algorithm
+
+function bubbleSort(array: number[]) {
+    for (let i = 1; i < array.length; i++) {
+        for (let j = array.length; j >= i; j--) {
+            if (array[j] < array[j - 1]) {
+                var aux = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = aux;
+            }
+        }
+    }
+}
+
+// TODO: I'm having problems with var names iqual in other files
+const array3 = [7, 6, 5, 4, 3, 2, 1];
+bubbleSort(array3);
+console.log(array3);
